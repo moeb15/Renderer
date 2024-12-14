1aad6b6bd4508e0c70d27507cf52a41cb12b1f59
Mujtaba Bader <mujtaba.bader@hotmail.com>
2024-12-14 08:12:31 -0500

    camera class

---
+++ DX11Renderer/src/Renderer/Camera.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <DirectXMath.h>
+
+namespace Yassin
+{
+	class Camera
+	{
+	public:
+		Camera(float FOV, float aspectRatio);
+
+		inline void SetPosition(float x, float y, float z)
+		{
+			m_Position.x = x;
+			m_Position.y = y;
+			m_Position.z = z;
+
+			UpdateView();
+		}
+
+		inline void SetRotation(float yaw, float pitch, float roll)
+		{
+			m_Yaw = yaw;
+			m_Pitch = pitch;
+			m_Roll = roll;
+
+			UpdateView();
+		}
+
+		inline DirectX::XMFLOAT3 GetPosition() const { return m_Position; };
+		inline DirectX::XMFLOAT3 GetRotation() { return DirectX::XMFLOAT3(m_Pitch, m_Yaw, m_Roll); }
+
+		inline void GetViewMatrix(DirectX::XMMATRIX& matrix) { matrix = DirectX::XMLoadFloat4x4(&m_ViewMatrix); }
+		inline void GetProjectionMatrix(DirectX::XMMATRIX& matrix) { matrix = DirectX::XMLoadFloat4x4(&m_ProjectionMatrix); }
+
+	private:
+		void UpdateView();
+
+	private:
+		DirectX::XMFLOAT3 m_Position;
+		float m_Yaw;
+		float m_Pitch;
+		float m_Roll;
+		DirectX::XMFLOAT4X4 m_ViewMatrix;
+		DirectX::XMFLOAT4X4 m_ProjectionMatrix;
+	};
+}
