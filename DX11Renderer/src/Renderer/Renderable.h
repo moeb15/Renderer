230bc9c3bcdc4035f180e496095209c37f5a39a2
Mujtaba Bader <mujtaba.bader@hotmail.com>
2024-12-17 04:05:53 -0500

    added transform buffer as protected member variable, methods for updating world transform

--- DX11Renderer/src/Renderer/Renderable.h
+++ DX11Renderer/src/Renderer/Renderable.h
@@ -33,7 +33,36 @@ namespace Yassin
 		const ObjectType& GetObjectType() const { return m_ObjectType; }
 		const ObjectVisibility GetObjectVisibility() const { return m_ObjectVisibility; }
 
+		void Translate(float x, float y, float z)
+		{
+			DirectX::XMMATRIX& world = m_TransformBuffer->GetWorld();
+			DirectX::XMMATRIX translation = DirectX::XMMatrixTranslation(x, y, z);
+			world = DirectX::XMMatrixMultiply(translation, world);
+			world = DirectX::XMMatrixTranspose(world);
+		}
+
+		void Rotate(float yaw, float pitch, float roll)
+		{
+			yaw = DirectX::XMConvertToRadians(yaw);
+			pitch = DirectX::XMConvertToRadians(pitch);
+			roll = DirectX::XMConvertToRadians(roll);
+
+			DirectX::XMMATRIX& world = m_TransformBuffer->GetWorld();
+			DirectX::XMMATRIX rotation = DirectX::XMMatrixRotationRollPitchYaw(pitch, yaw, roll);
+			world = DirectX::XMMatrixMultiply(world, rotation);
+			world = DirectX::XMMatrixTranspose(world);
+		}
+
+		void Scale(float x, float y, float z)
+		{
+			DirectX::XMMATRIX& world = m_TransformBuffer->GetWorld();
+			DirectX::XMMATRIX scale = DirectX::XMMatrixScaling(x, y, z);
+			world = DirectX::XMMatrixMultiply(scale, world);
+			world = DirectX::XMMatrixTranspose(world);
+		}
+
 	protected:
+		std::shared_ptr<TransformBuffer> m_TransformBuffer;
 		ObjectType m_ObjectType = ObjectType::Geometry;
 		ObjectVisibility m_ObjectVisibility = ObjectVisibility::Opaque;
 	};
