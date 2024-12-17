a034dfba2a3bb7077b7d7e53166cefda8c83b916
Mujtaba Bader <mujtaba.bader@hotmail.com>
2024-12-17 08:26:08 -0500

    texture library class

---
+++ DX11Renderer/src/Renderer/TextureLibrary.cpp
@@ -0,0 +1,42 @@
+#include "Renderer/TextureLibrary.h"
+
+namespace Yassin
+{
+	TextureLibrary* TextureLibrary::s_Instance = nullptr;
+
+	TextureLibrary::TextureLibrary()
+	{
+		assert(s_Instance == nullptr);
+		s_Instance = this;
+	}
+
+	void TextureLibrary::Init()
+	{
+		s_Instance->m_Texture2DMap.clear();
+	}
+
+	void TextureLibrary::Add(const std::string& name, const std::string& textureFile, TextureType type)
+	{
+		switch(type)
+		{
+			case TextureType::Tex1D: 
+				break;
+
+			case TextureType::Tex2D:
+				if (s_Instance->m_Texture2DMap.find(name) != s_Instance->m_Texture2DMap.end()) return;
+
+				std::unique_ptr<Texture2D> tex2D = std::make_unique<Texture2D>();
+				tex2D->Init(textureFile.c_str());
+				s_Instance->m_Texture2DMap.emplace(name, std::move(tex2D));
+				break;
+		}
+	}
+
+	Texture2D* TextureLibrary::GetTexture2D(const std::string& name)
+	{
+		if (s_Instance->m_Texture2DMap.find(name) == s_Instance->m_Texture2DMap.end()) return nullptr;
+
+		return s_Instance->m_Texture2DMap.find(name)->second.get();
+	}
+
+}
---
+++ DX11Renderer/src/Renderer/TextureLibrary.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Renderer/Texture.h"
+#include <string>
+#include <unordered_map>
+#include <memory>
+
+namespace Yassin
+{
+	enum class TextureType
+	{
+		Tex1D,
+		Tex2D,
+		Tex3D,
+	};
+	class TextureLibrary
+	{
+	public:
+		TextureLibrary();
+
+		static void Init();
+		static void Add(const std::string& name, const std::string& textureFile, TextureType type);
+		static Texture2D* GetTexture2D(const std::string& name);
+
+	private:
+		static TextureLibrary* s_Instance;
+		std::unordered_map<std::string, std::unique_ptr<Texture2D>> m_Texture2DMap;
+	};
+}
