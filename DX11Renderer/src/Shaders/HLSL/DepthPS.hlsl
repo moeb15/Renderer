892d21773345999dff398a84463b9878e1cb3872
Mujtaba Bader <mujtaba.bader@hotmail.com>
2024-12-20 13:57:09 -0500

    shader for visualizing depth buffer values

---
+++ DX11Renderer/src/Shaders/HLSL/DepthPS.hlsl
@@ -0,0 +1,16 @@
+struct PSIn
+{
+    float4 position : SV_POSITION;
+    float4 depthPos : DEPTHPOS;
+};
+
+float4 main(PSIn input) : SV_TARGET
+{
+    float depthValue;
+    float4 color;
+    
+    depthValue = input.depthPos.z / input.depthPos.w;
+    color = float4(depthValue, depthValue, depthValue, 1.0f);
+    
+    return color;
+}
---
+++ DX11Renderer/src/Shaders/HLSL/DepthVS.hlsl
@@ -0,0 +1,28 @@
+cbuffer MatrixBuffer
+{
+    row_major matrix world;
+    row_major matrix viewProj;
+};
+
+struct VSIn
+{
+    float4 position : POSITION;
+};
+
+struct VSOut
+{
+    float4 position : SV_POSITION;
+    float4 depthPos : DEPTHPOS;
+};
+
+VSOut main(VSIn input)
+{
+    VSOut vso;
+    
+    vso.position = mul(float4(input.position.xyz, 1.0f), world);
+    vso.position = mul(vso.position, viewProj);
+    
+    vso.depthPos = mul(float4(input.position.xyz, 1.0f), world);
+    
+	return vso;
+}
