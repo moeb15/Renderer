c3d179883f4e24df9ca73153686b434a842315ac
Mujtaba Bader <mujtaba.bader@hotmail.com>
2024-12-23 07:27:20 -0500

    rendertotexture class, can be used for post processing, shadow mapping, etc

---
+++ DX11Renderer/src/Renderer/RenderToTexture.cpp
@@ -0,0 +1,113 @@
+#include "Renderer/RenderToTexture.h"
+
+
+namespace Yassin
+{
+	void RenderToTexture::Init(unsigned int renderWidth, unsigned int renderHeight, float sNear, float sFar)
+	{
+		m_Width = renderWidth;
+		m_Height = renderHeight;
+
+		HRESULT hr;
+		D3D11_TEXTURE2D_DESC tDesc = {};
+		tDesc.Width = renderWidth;
+		tDesc.Height = renderHeight;
+		tDesc.MipLevels = 1;
+		tDesc.ArraySize = 1;
+		tDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		tDesc.SampleDesc.Count = 1;
+		tDesc.SampleDesc.Quality = 0;
+		tDesc.Usage = D3D11_USAGE_DEFAULT;
+		tDesc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
+
+		hr = RendererContext::GetDevice()->CreateTexture2D(&tDesc, nullptr, &m_RTT);
+		if (FAILED(hr))
+		{
+			MessageBox(RendererContext::GetWindowHandle(), L"Failed to create render target texture", L"RenderToTexture", MB_OK);
+			return;
+		}
+
+		D3D11_RENDER_TARGET_VIEW_DESC rDesc = {};
+		rDesc.Format = tDesc.Format;
+		rDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+		rDesc.Texture2D.MipSlice = 0;
+
+		hr = RendererContext::GetDevice()->CreateRenderTargetView(m_RTT.Get(), &rDesc, &m_RTV);
+		if (FAILED(hr))
+		{
+			MessageBox(RendererContext::GetWindowHandle(), L"Failed to create render target view", L"RenderToTexture", MB_OK);
+			return;
+		}
+
+		D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+		srvDesc.Format = tDesc.Format;
+		srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+		srvDesc.Texture2D.MostDetailedMip = 0;
+		srvDesc.Texture2D.MipLevels = 1;
+
+		hr = RendererContext::GetDevice()->CreateShaderResourceView(m_RTT.Get(), &srvDesc, &m_SRV);
+		if (FAILED(hr))
+		{
+			MessageBox(RendererContext::GetWindowHandle(), L"Failed to create shader resource view", L"RenderToTexture", MB_OK);
+			return;
+		}
+
+		D3D11_TEXTURE2D_DESC dDesc = {};
+		dDesc.Width = m_Width;
+		dDesc.Height = m_Height;
+		dDesc.MipLevels = 1;
+		dDesc.ArraySize = 1;
+		dDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+		dDesc.SampleDesc.Count = 1;
+		dDesc.SampleDesc.Quality = 0;
+		dDesc.Usage = D3D11_USAGE_DEFAULT;
+		dDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+
+		hr = RendererContext::GetDevice()->CreateTexture2D(&dDesc, nullptr, &m_DSB);
+		if (FAILED(hr))
+		{
+			MessageBox(RendererContext::GetWindowHandle(), L"Failed to create depth stencil buffer", L"RenderToTexture", MB_OK);
+			return;
+		}
+
+		D3D11_DEPTH_STENCIL_VIEW_DESC dsvDesc = {};
+		dsvDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+		dsvDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+		dsvDesc.Texture2D.MipSlice = 0;
+
+		hr = RendererContext::GetDevice()->CreateDepthStencilView(m_DSB.Get(), &dsvDesc, &m_DSV);
+		if (FAILED(hr))
+		{
+			MessageBox(RendererContext::GetWindowHandle(), L"Failed to create depth stencil view", L"RenderToTexture", MB_OK);
+			return;
+		}
+
+		m_VP.Width = (float)renderWidth;
+		m_VP.Height = (float)renderHeight;
+		m_VP.MinDepth = 0.0f;
+		m_VP.MaxDepth = 1.0f;
+		m_VP.TopLeftX = 0;
+		m_VP.TopLeftY = 0;
+
+		DirectX::XMMATRIX proj =
+			DirectX::XMMatrixPerspectiveFovLH(DirectX::XMConvertToRadians(75.f), (float)renderWidth / (float)renderHeight, sNear, sFar);
+		DirectX::XMStoreFloat4x4(&m_ProjMatrix, proj);
+
+		DirectX::XMMATRIX ortho =
+			DirectX::XMMatrixOrthographicLH((float)renderWidth, (float)renderHeight, sNear, sFar);
+		DirectX::XMStoreFloat4x4(&m_OrthoMatrix, ortho);
+	}
+
+	void RenderToTexture::SetRenderTarget()
+	{
+		RendererContext::GetDeviceContext()->OMSetRenderTargets(1, m_RTV.GetAddressOf(), m_DSV.Get());
+	}
+	
+	void RenderToTexture::ClearRenderTarget(float r, float g, float b, float a)
+	{
+		float color[] = { r,g,b,a };
+		RendererContext::GetDeviceContext()->ClearRenderTargetView(m_RTV.Get(), color);
+		RendererContext::GetDeviceContext()->ClearDepthStencilView(m_DSV.Get(), 
+			D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+	}
+}
---
+++ DX11Renderer/src/Renderer/RenderToTexture.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Renderer/RendererContext.h"
+
+namespace Yassin
+{
+	class RenderToTexture
+	{
+	public:
+		void Init(unsigned int renderWidth, unsigned int renderHeight,
+			float sNear, float sFar);
+
+		void SetRenderTarget();
+		void ClearRenderTarget(float r, float g, float b, float a);
+
+		inline ID3D11ShaderResourceView* GetSRV() { return m_SRV.Get(); }
+
+		inline void GetPerspectiveMatrix(DirectX::XMMATRIX& matrix) const { matrix = DirectX::XMLoadFloat4x4(&m_ProjMatrix); }
+		inline void GetOrthoMatrix(DirectX::XMMATRIX& matrix) const { matrix = DirectX::XMLoadFloat4x4(&m_OrthoMatrix); }
+
+		inline unsigned int GetWidth() const { return m_Width; }
+		inline unsigned int GetHeight() const { return m_Height; }
+
+	private:
+		unsigned int m_Width;
+		unsigned int m_Height;
+
+		Microsoft::WRL::ComPtr<ID3D11Texture2D> m_RTT;
+		Microsoft::WRL::ComPtr<ID3D11RenderTargetView> m_RTV;
+		Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_SRV;
+		Microsoft::WRL::ComPtr<ID3D11Texture2D> m_DSB;
+		Microsoft::WRL::ComPtr<ID3D11DepthStencilView> m_DSV;
+		D3D11_VIEWPORT m_VP;
+
+		DirectX::XMFLOAT4X4 m_ProjMatrix;
+		DirectX::XMFLOAT4X4 m_OrthoMatrix;
+	};
+}
+
