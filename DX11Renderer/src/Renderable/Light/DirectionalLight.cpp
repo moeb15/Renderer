8e295880092aa66e10d392b42c85fb29f2b66339
Mujtaba Bader <mujtaba.bader@hotmail.com>
2025-01-02 18:54:42 -0500

    directional light

---
+++ DX11Renderer/src/Renderable/Light/DirectionalLight.cpp
@@ -0,0 +1,17 @@
+#include "Renderable/Light/DirectionalLight.h"
+
+namespace Yassin
+{
+	void DirectionalLight::UpdateView()
+	{
+		DirectX::XMFLOAT3 up(0.0f, 1.0f, 0.0f);
+		DirectX::XMVECTOR upVec, lookAtVec, posVec;
+
+		upVec = DirectX::XMLoadFloat3(&up);
+		lookAtVec = DirectX::XMLoadFloat3(&m_LookAt);
+		posVec = DirectX::XMLoadFloat3(&m_Position);
+
+		DirectX::XMMATRIX view = DirectX::XMMatrixLookAtLH(posVec, lookAtVec, upVec);
+		DirectX::XMStoreFloat4x4(&m_ViewMatrix, view);
+	}
+}
