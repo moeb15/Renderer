a769c5b750802bb1144fb764e775a267a5624886
Mujtaba Bader <mujtaba.bader@hotmail.com>
2025-01-13 04:51:54 -0500

    added setter

--- DX11Renderer/src/Renderer/Primitives/Topology.h
+++ DX11Renderer/src/Renderer/Primitives/Topology.h
@@ -6,7 +6,7 @@ namespace Yassin
 	class Topology
 	{
 	public:
-		Topology(D3D11_PRIMITIVE_TOPOLOGY topology) : m_Topology(topology) {}
+		Topology(D3D11_PRIMITIVE_TOPOLOGY topology = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST) : m_Topology(topology) {}
 		
 		inline void Bind() const
 		{
@@ -14,6 +14,7 @@ namespace Yassin
 		}
 
 		inline D3D11_PRIMITIVE_TOPOLOGY GetTopology() const { return m_Topology; }
+		inline void SetTopology(D3D11_PRIMITIVE_TOPOLOGY topology) { m_Topology = topology; }
 
 	private:
 		D3D11_PRIMITIVE_TOPOLOGY m_Topology;
