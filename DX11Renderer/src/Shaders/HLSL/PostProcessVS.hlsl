dd941baeb910ce59c64d164d427a813b702f7211
Mujtaba Bader <mujtaba.bader@hotmail.com>
2025-01-16 04:42:06 -0500

    renamed defaultvs

--- DX11Renderer/src/Shaders/HLSL/PostProcessVS.hlsl
+++ DX11Renderer/src/Shaders/HLSL/PostProcessVS.hlsl
@@ -9,6 +9,7 @@ struct VSIn
     float4 position : POSITION;
     float2 uv : TEXCOORD;
     float3 normal : NORMAL;
+    float3 instancePosition : INSTANCEPOS;
 };
 
 struct VSOut
@@ -23,6 +24,10 @@ VSOut main(VSIn input)
     VSOut vso;
     
     input.position.w = 1.0f;
+    input.position.x += input.instancePosition.x;
+    input.position.y += input.instancePosition.y;
+    input.position.z += input.instancePosition.z;
+    
     vso.position = mul(input.position, world);
     vso.position = mul(vso.position, viewProj);
     
