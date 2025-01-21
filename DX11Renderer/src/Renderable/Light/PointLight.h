4f4bc44c039ddb9797ca7d1d2a9b0c2db2e4ac4c
Mujtaba Bader <mujtaba.bader@hotmail.com>
2025-01-21 15:29:57 -0500

    added bounding sphere so lights can be culled

--- DX11Renderer/src/Renderable/Light/PointLight.h
+++ DX11Renderer/src/Renderable/Light/PointLight.h
@@ -1,12 +1,12 @@
 #pragma once
-#include <DirectXMath.h>
+#include <DirectXCollision.h>
 
 namespace Yassin
 {
 	class PointLight
 	{
 	public:
-		PointLight(float FOV, float aspectRatio, float sNear, float sFar) :
+		PointLight(float FOV, float aspectRatio, float sNear, float sFar, float radius = 1.0f) :
 			m_AmbientColor(0.1f, 0.1f, 0.1f, 1.0f),
 			m_DiffuseColor(1.0f, 1.0f, 1.0f, 1.0f),
 			m_SpecularColor(1.f, 1.f, 1.f, 1.0f),
@@ -21,6 +21,8 @@ namespace Yassin
 				sFar);
 			DirectX::XMStoreFloat4x4(&m_ProjectionMatrix, proj);
 			UpdateView();
+
+			m_BoundingSphere = DirectX::BoundingSphere(DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f), radius);
 		}
 
 		inline void SetDiffuseColor(float r, float g, float b, float a)
@@ -43,6 +45,8 @@ namespace Yassin
 		inline void SetPosition(float x, float y, float z)
 		{
 			m_Position = DirectX::XMFLOAT3(x, y, z);
+
+			m_BoundingSphere.Transform(m_BoundingSphere, DirectX::XMMatrixTranslation(x, y, z));
 			UpdateView();
 		}
 
@@ -61,6 +65,7 @@ namespace Yassin
 
 		inline void GetView(DirectX::XMMATRIX& matrix) const { matrix = DirectX::XMLoadFloat4x4(&m_ViewMatrix); }
 		inline void GetProjection(DirectX::XMMATRIX& matrix) const { matrix = DirectX::XMLoadFloat4x4(&m_ProjectionMatrix); }
+		inline const DirectX::BoundingSphere& GetBoundingSphere() { return m_BoundingSphere; }
 
 	private:
 		void UpdateView();
@@ -74,5 +79,7 @@ namespace Yassin
 		DirectX::XMFLOAT3 m_LookAt;
 		DirectX::XMFLOAT4X4 m_ViewMatrix;
 		DirectX::XMFLOAT4X4 m_ProjectionMatrix;
+
+		DirectX::BoundingSphere m_BoundingSphere;
 	};
 }
