651bdffe6e11379320f1d3bcd60674a06ac8c419
Mujtaba Bader <mujtaba.bader@hotmail.com>
2025-01-23 12:12:55 -0500

    directional shadow map shaders

---
+++ DX11Renderer/src/Shaders/HLSL/ShadowMapDirPS.hlsl
@@ -0,0 +1,64 @@
+Texture2D depthMapTexture : register(t4);
+SamplerState clampSampler : register(s0);
+
+cbuffer LightPropsBuffer : register(b0)
+{
+    float4 ambientColor;
+    float4 diffuseColor;
+    float4 specularColor;
+    float specularPower;
+    float bias;
+    float2 padding;
+};
+
+cbuffer LightDirectionBuffer : register(b2)
+{
+    float3 lightDirection;
+    float padding0;
+};
+
+struct PSIn
+{
+    float4 position : SV_POSITION;
+    float2 uv : TEXCOORD;
+    float3 normal : NORMAL;
+    float4 lightViewPos : VIEWPOS;
+    float3 lightPos : LIGHTPOS;
+};
+
+float4 main(PSIn input) : SV_TARGET
+{
+    float4 finalColor;
+    float2 projectedUV;
+    float depthValue;
+    float lightDepthValue;
+    float lightIntensity;
+    float3 lightDir;
+    
+    lightDir = -lightDirection;
+    
+    finalColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+    
+    projectedUV.x = (input.lightViewPos.x / input.lightViewPos.w + 1.0f) / 2.0f;
+    projectedUV.y = (-input.lightViewPos.y / input.lightViewPos.w + 1.0f) / 2.0f;
+    
+    if ((saturate(projectedUV.x) == projectedUV.x) && (saturate(projectedUV.y) == projectedUV.y))
+    {
+        depthValue = depthMapTexture.Sample(clampSampler, projectedUV).r;
+        
+        lightDepthValue = input.lightViewPos.z / input.lightViewPos.w;
+        lightDepthValue -= bias;
+        
+        if (lightDepthValue < depthValue)
+        {
+            lightIntensity = saturate(dot(input.normal, lightDir));
+            
+            if (lightIntensity > 0.0f)
+            {
+                finalColor = float4(1.0f, 1.0f, 1.0f, 1.0f);
+            }
+        }
+    }
+    
+    return finalColor;
+}
